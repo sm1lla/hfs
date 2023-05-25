bf4797b15073e8a36ff04ad893f3611ca8d6f172
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-05-25 11:47:03 +0200

    TSEL feature selection untested

---
+++ hfs/feature_selection.py
@@ -0,0 +1,75 @@
+"""
+Sklearn compatible estimators for feature selection
+"""
+import networkx as nx
+import numpy as np
+from networkx.algorithms.dag import descendants
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_X_y
+
+from .helpers import create_feature_tree, get_paths, lift
+
+
+class TreeBasedFeatureSelector(TransformerMixin, BaseEstimator):
+    """A tree-based feature selection method for hierarchical features"""
+
+    def __init__(self, hierarchy: nx.DiGraph, columns: list[str]):
+        assert nx.is_directed_acyclic_graph(
+            hierarchy
+        ), "The hierarchy needs to be an directed acyclic graph."
+
+        self.feature_tree = create_feature_tree(hierarchy)
+        self.columns = columns
+        self.representatives = None
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """A reference implementation of a fitting function for a transformer.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,)
+            The target values. An array of int, that should either be 1 or 0.
+
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        X, y = check_X_y(X, y)
+
+        paths = get_paths()
+        get_lift_values = lift(X, y)
+        self.node_to_lift = {
+            self.columns[index]: get_lift_values[index]
+            for index, _ in enumerate(self.columns)
+        }
+        self.representatives = self.find_representatives(paths)
+
+        return self
+
+    def transform(self, X: np.ndarray):
+        column_indices = [self.columns.index(node) for node in self.representatives]
+        columns = [X[:, index] for index in column_indices]
+        return np.column_stack(columns)
+
+    def find_representatives(self, paths: list[list[str]]):
+        representatives = set()
+        for path in paths:
+            path.remove("ROOT")
+            max_node = max(path, key=lambda x: self.node_to_lift[x])
+            representatives.append(max_node)
+        return self._filter_representatives(representatives)
+
+    def _filter_representatives(self, representatives: list[str]):
+        updated_representatives = []
+        for node in representatives:
+            selected_decendents = [
+                descendent
+                for descendent in descendants(self.feature_tree, node)
+                if descendent in representatives
+            ]
+            if not selected_decendents:
+                updated_representatives.append(node)
+        return updated_representatives
---
+++ hfs/helpers.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import numpy as np
+from networkx.algorithms.simple_paths import all_simple_paths
+
+
+def create_feature_tree(hierarchy: nx.DiGraph) -> nx.DiGraph:
+    # create parent node to join hierarchies
+    roots = [x for x in hierarchy.nodes() if hierarchy.in_degree(x) == 0]
+
+    for root_node in roots:
+        hierarchy.add_edge("ROOT", root_node)
+
+
+def get_paths(graph: nx.DiGraph):
+    leafs = [
+        node
+        for node in graph
+        if graph.in_degree(node) > 0 and graph.out_degree(node) == 0
+    ]
+
+    paths = all_simple_paths(graph, "ROOT", leafs)
+    return paths
+
+
+def lift(data: np.ndarray, labels: np.ndarray):
+    """returns list including lift value for each feature"""
+    lift_values = []
+    num_samples, num_features = data.shape
+
+    for index in range(num_features):
+        column = data[:, index]
+        prob_feature = len(np.nonzero(column)) / num_samples
+        prob_event_conditional = len(
+            [
+                value
+                for index, value in enumerate(column)
+                if value != 0 and labels[index] != 0
+            ]
+        ) / len(np.nonzero(column))
+
+        lift_values.append(prob_event_conditional / prob_feature)
