a9269d5f389b32c2a8f231d07d4c97a2283b6358
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-06-01 09:00:52 +0200

    select from path implementation + hierarchy format
    - Implemented two variations of the select_from_path algorithm (from paper peudocode and other interpretation)
    - Pass hierarchy as numpy array and transform it to a nx.Digraph in the feature selector class

--- hfs/tests/test_feature_selection.py
+++ hfs/tests/test_feature_selection.py
@@ -19,7 +19,7 @@ def data():
         ],
         columns=columns,
     )
-    edges = [("A", "B"), ("B", "C"), ("A", "D"), ("A", "E")]
+    edges = [(0, 1), (1, 2), (0, 3), (0, 4)]
     hierarchy = nx.DiGraph(edges)
     y = np.array([0, 0, 0, 0, 1])
     X = df.to_numpy()
@@ -28,7 +28,7 @@ def data():
 
 def test_tree_based_selection(data):
     X, y, hierarchy, columns = data
-    selector = TreeBasedFeatureSelector(hierarchy)
-    selector.fit(X, y, columns)
+    selector = TreeBasedFeatureSelector(nx.to_numpy_array(hierarchy))
+    selector.fit(X, y)
     X = selector.transform(X)
     assert np.array_equal(X, np.array([[0], [0], [0], [0], [1]]))
