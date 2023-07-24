40828aeaef42d3e96419bf549f1a1959fc02364e
sm1lla <foxsmilla@gmail.com>
2023-07-24 08:13:26 +0000

    log experiments with wandb

--- classify.py
+++ classify.py
@@ -1,3 +1,21 @@
 from experiments.experiments_eager import classification_experiments
+import argparse
 
-classification_experiments()
+
+def main():
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument(
+        "-e", "--experiments", type=str, nargs="+", help="Experiments to run"
+    )
+    argParser.add_argument("--wandb", action=argparse.BooleanOptionalAction)
+
+    args = argParser.parse_args()
+
+    if args.experiments == None:
+        classification_experiments(use_wandb=args.wandb)
+    else:
+        classification_experiments(use_wandb=args.wandb, experiments=args.experiments)
+
+
+if __name__ == "__main__":
+    main()
--- experiments/experiments_eager.py
+++ experiments/experiments_eager.py
@@ -8,6 +8,8 @@ from hfs.hill_climbing import BottomUpSelector, TopDownSelector
 from hfs.preprocessing import HierarchicalPreprocessor
 from hfs.shsel import SHSELSelector
 from hfs.tsel import TSELSelector
+import time
+import wandb
 
 
 def get_preprocessed_data():
@@ -73,23 +75,85 @@ def gtd(X, y, hierarchy, columns):
     return X_transformed
 
 
+def baseline(X, y, hierarchy, columns):
+    print("Baseline (without feature selection)")
+    return X
+
+
 def classify(X, y, classifier):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=0
     )
     classifier.fit(X_train, y_train)
     accuracy = classifier.score(X_test, y_test)
-    print(f"Accuracy: {accuracy}")
+    return accuracy
 
 
-def classification_experiments():
-    experiments = [shsel, hfe, tsel, gtd, bottom_up, top_down]
+def initialize_wandb(experiment: str):
+    wandb.init(
+        project="hfs",
+        config={
+            "experiment": experiment,
+        },
+    )
 
+
+def get_experiment():
+    return {
+        "shsel": shsel,
+        "hfe": hfe,
+        "tsel": tsel,
+        "bottom_up": bottom_up,
+        "top_down": top_down,
+        "gtd": gtd,
+        "baseline": baseline,
+    }
+
+
+def classification_experiments(
+    use_wandb=True,
+    experiments: list[str] = [
+        "baseline",
+        "shsel",
+        "hfe",
+        "tsel",
+        "gtd",
+        "bottom_up",
+        "top_down",
+    ],
+):
     classifier = GaussianNB()
-    print("Baseline without feature extraction")
     X, y, hierarchy, columns = get_preprocessed_data()
-    classify(X, y, classifier)
 
-    for experiment in experiments:
+    for experiment_name in experiments:
+        if use_wandb:
+            initialize_wandb(experiment_name)
+        experiment = get_experiment()[experiment_name]
+        start_time = time.time()
         X_transformed = experiment(X, y, hierarchy, columns)
-        classify(X_transformed, y, classifier)
+        transform_time = time.time()
+        accuracy = classify(X_transformed, y, classifier)
+        classfiy_time = time.time()
+
+        # Calculate metrics
+        compression_rate = X.shape[1] / X_transformed.shape[1]
+        preprocess_time = transform_time - start_time
+        num_features = X_transformed.shape[1]
+        classify_time = classfiy_time - transform_time
+
+        # Print metrics
+        print(f"Accuracy: {accuracy}")
+        print(f"Compression rate: {compression_rate}")
+
+        # Loggging
+        if use_wandb:
+            wandb.log(
+                {
+                    "accuracy": accuracy,
+                    "compression_rate": compression_rate,
+                    "preprocess_time": preprocess_time,
+                    "classify_time": classify_time,
+                    "num_features": num_features,
+                }
+            )
+            wandb.finish()
