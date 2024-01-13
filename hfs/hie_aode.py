0ededd3a8c44270af79f2ba2b0c779416cdb4396
sara.grau <sara.grau@4-cast.de>
2024-01-13 19:18:12 +0100

    refactored HieAODE to calculate each of the conditional probabilities with helper functions

--- hfs/hie_aode.py
+++ hfs/hie_aode.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import networkx as nx
 import numpy as np
 from sklearn.naive_bayes import BernoulliNB
@@ -18,7 +20,27 @@ class HieAODE(LazyHierarchicalFeatureSelector):
         hierarchy : np.ndarray
             The hierarchy graph as an adjacency matrix.
         """
+        self.cpts = dict()
         super(HieAODE, self).__init__(hierarchy)
+
+    def fit_selector(self, X_train, y_train, X_test, columns=None):
+        """
+        P (y, x_i )
+        class_prior
+
+        self.n_ancestors = self.n_descendants = self.n_features
+        P (x_k|y)
+        ascendants_class_cpt = (self.n_ancestors, self.n_classes, self.n_features, n_values)
+
+        P (x_j|y, x_i)
+        feature_descendants_class_cpt = (self.n_features, self.n_descendants, self.n_classes, n_values)
+        """
+        super(HieAODE, self).fit_selector(X_train, y_train, X_test, columns)
+        self.cpts = dict(
+            prior=np.full((self.n_features, self.n_classes, 2), -1),  # P(y, x_i )
+            descendants=np.full((self.n_features, self.n_features, self.n_classes, 2), -1),  # P(x_j|y, x_i)
+            ascendants=np.full((self.n_features, self.n_classes, 2), -1)  # P(x_k|y)
+        )
 
     def select_and_predict(
         self, predict=True, saveFeatures=False, estimator=BernoulliNB()
@@ -42,47 +64,51 @@ class HieAODE(LazyHierarchicalFeatureSelector):
         predictions for test input samples, if predict = false, returns empty array.
         """
 
-        self.cpts = []
-        for sample in range(self._xtest.shape[0]):
-            x_i = self._xtest[sample]
-            class_prior = np.zeros((self.n_classes, self.n_features, 2))
+        for sample_idx in range(self._xtest.shape[0]):
+            sample = self._xtest[sample_idx]
+            for feature_idx in range(len(sample)):
+                self.calculate_class_prior(sample=sample, feature_idx=feature_idx)
 
-            for feature_idx in range(len(x_i)):
-                for c in range(self.n_classes):
-                    for v in range(2):
-                        class_prior[c][feature_idx][v] = (
-                            np.sum(self._ytrain == c and x_i == v)
-                            / self._ytrain.shape[0]
-                        )
+                ascendants = nx.ancestors(self._hierarchy, feature_idx)
+                for a in range(len(ascendants)):
+                    self.calculate_prob_given_ascendant_class(ascendant=a)
 
-                ancestors = nx.ancestors(self._hierarchy, feature_idx)
-                n_ancestors = len(ancestors)
-                n_descendents = len(nx.descendants(self._hierarchy, feature_idx))
+                descendants = nx.descendants(self._hierarchy, feature_idx)
+                # Remove elements from descendants that are in ascendants
+                descendants = [item for item in descendants if item not in ascendants]
+                for descendant_idx in range(len(descendants)):
+                    self.calculate_prob_descendant_given_class_feature(descendant_idx=descendant_idx, feature_idx=feature_idx)
+    def calculate_class_prior(self, sample, feature_idx):
+        for c in range(self.n_classes):
+            for value in range(2):
+                self.cpts["prior"][feature_idx][c][value] = (
+                        (np.sum(self._ytrain == c) & (sample == value))
+                        / self._ytrain.shape[0]
+                )
 
-                ancestors_class_cpt = [
-                    np.zeros((self.n_classes, self.n_features, 2))
-                    for _ in range(n_ancestors)
-                ]
+    def calculate_prob_given_ascendant_class(self, ascendant):
+        # Calculate P(x_k | y) where x_k=ascendant and y = c
+        for c in range(self.n_classes):
+            for value in range(2):
+                p_class_ascendant = np.sum(
+                    (self._ytrain == c) & (self._xtrain[:, ascendant] == value)
+                )
+                p_class = np.sum(self._ytrain == c)
+                self.cpts["ascendants"][ascendant][c][value] = p_class_ascendant / p_class
 
-                for a in range(n_ancestors):
-                    for c in range(self.n_classes):
-                        for v in range(2):
-                            ancestors_class_cpt[a][c][feature_idx][v] = np.sum(
-                                self._ytrain == c
-                                and self._xtrain[sample, feature_idx] == v
-                            ) / np.sum(self._ytrain == c)
+    def calculate_prob_descendant_given_class_feature(self, descendant_idx, feature_idx):
+        for c in range(self.n_classes):
+            for value in range(2):
+                if descendant_idx != feature_idx:
+                    descendant = self._xtrain[:, descendant_idx]
 
-                descendents_class_cpt = [
-                    np.zeros((self.n_classes, self.n_features, 2))
-                    for _ in range(n_descendents)
-                ]
+                    # Calculate P(x_j | y, x_i = value)
+                    mask = (feature_idx == value) & (self._ytrain == c)
+                    total = np.sum(mask)
 
-                # Todo calculate descendents
+                    if total > 0:
+                        prob_descendant_given_c_feature = np.sum(descendant[mask]) / total
+                    else:
+                        prob_descendant_given_c_feature = 0
 
-            self.cpts.append(
-                {
-                    "prior": class_prior,
-                    "ancestors": ancestors_class_cpt,
-                    "descendents": descendents_class_cpt,
-                }
-            )
+                    self.cpts["descendants"][descendant][feature_idx][c][value] = prob_descendant_given_c_feature
---
+++ hfs/tests/test_hie_aode.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pytest
+
+from hfs.hie_aode import HieAODE
+from .fixtures.fixtures import *
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        lazy_data2(),
+    ],
+)
+def test_hie_aode(data):
+    small_DAG, train_x_data, train_y_data, test_x_data, test_y_data = data
+    selector = HieAODE(hierarchy=small_DAG)
+    selector.fit_selector(X_train=train_x_data, y_train=train_y_data, X_test=test_x_data)
+    pred = selector.select_and_predict(predict=True, saveFeatures=True)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        lazy_data2(),
+    ],
+)
+def test_calculate_dependency_ascendant_class(data):
+    small_DAG, train_x_data, train_y_data, test_x_data, test_y_data = data
+    selector = HieAODE(hierarchy=small_DAG)
+    selector.fit_selector(X_train=train_x_data, y_train=train_y_data, X_test=test_x_data)
+    sample_idx=1
+    sample = test_x_data[1]
+    feature_idx=2
+    expected=np.full((selector.n_features, selector.n_classes, 2), -1)
+    expected[0][0][0] = 0.0
+    expected[0][1][0] = 0.0
+    expected[0][0][1] = 1.0
+    expected[0][1][1] = 1.0
+    expected[1][0][0] = 0.0
+    expected[1][1][0] = 0.0
+    expected[1][0][1] = 0.0
+    expected[1][1][1] = 1.0
+    ascendants = nx.ancestors(selector._hierarchy, feature_idx)
+
+    for a in range(len(ascendants)):
+        selector.calculate_prob_given_ascendant_class(ascendant=a)
+    assert np.array_equal(selector.cpts["ascendants"], expected)
