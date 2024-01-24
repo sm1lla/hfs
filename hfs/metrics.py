53959cab0eb415680b5273aa9e298b42cf423081
Jasperhino <blum.jasper@gmail.com>
2024-01-24 14:55:20 +0100

    add missing lib file

--- hfs/metrics.py
+++ hfs/metrics.py
@@ -8,7 +8,7 @@ from info_gain.info_gain import info_gain, info_gain_ratio
 from numpy.linalg import norm
 from scipy import sparse
 
-from .lib.pyitlib import information_mutual_conditional as imc
+from hfs.lib.pyitlib import information_mutual_conditional as imc
 
 
 def lift(data, labels):
